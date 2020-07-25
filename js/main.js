c560867100198a6358f4a0bb719a633a8a667410
karim <kokekarim@yahoo.com>
2020-07-25 21:47:19 +0200

    add logic to the menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
+let nav = document.querySelector("nav");
 let navMenu = document.querySelector("svg");
+let navUl = document.querySelector("#nav_ul");
+let navlis = document.querySelectorAll("ul li a");
+console.log(navlis);
 let toggleMenu = false;
+
 navMenu.addEventListener("click", () => {
-    // navMenu.classList.add()
-    console.log("helo")
+  if (!toggleMenu) {
+    nav.classList.add("grid-col-2");
+    navUl.classList.add("menu");
+    navUl.classList.add("flex-start-nav");
+    navUl.style.opacity = "1";
+    navlis.forEach(el => {
+      el.style.visibility = "visible";
+    });
+    toggleMenu = true;
+  } else {
+    nav.classList.remove("grid-col-2");
+    navUl.classList.remove("menu");
+    navUl.classList.remove("flex-start-nav");
+
+    navlis.forEach(el => {
+      el.style.visibility = "hidden";
+    });
+    toggleMenu = false;
+  }
 });
